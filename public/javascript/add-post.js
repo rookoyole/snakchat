cbd32fcb6a238642d1a9b281f292f6420098c351
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-05-12 14:25:21 -0700

    upload button works, dashboard doesnt display post images

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,34 +4,22 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_url = document.querySelector('input[name="post-url"]').value;
   const post_img = localStorage.getItem('imageURL')
-  var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'my_cloud_name', 
-    uploadPreset: 'my_preset'}, (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Done! Here is the image info: ', result.info); 
-      }
-   }
-  )
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_url,
+      post_img
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-  document.getElementById("upload_widget").addEventListener("click", function(){
-    myWidget.open();
-    }, false);
-}
-const response = await fetch(`/api/posts`, {
-  method: 'POST',
-  body: JSON.stringify({
-    title,
-    post_url,
-    post_img
-  }),
-  headers: {
-    'Content-Type': 'application/json'
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
   }
-});
-
-if (response.ok) {
-  document.location.replace('/dashboard');
-} else {
-  alert(response.statusText);
-}
+} 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
